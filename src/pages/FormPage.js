6c42a7032b0c139009a600457ef82727df0f3518
SAIKUMAR124 <pavankumar.s12459@gmail.com>
2021-11-12 20:33:27 +0530

    Added form validation

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -1,88 +1,134 @@
-import { useFormik } from 'formik';
-import React, { useState } from 'react'
-import { connect } from 'react-redux';
-import { setFormData } from '../redux';
-import SuccessPage from './SuccessPage';
-import './FormPage.css';
-import { Redirect } from 'react-router';
-import { Link } from 'react-router-dom';
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router";
+import { Link } from "react-router-dom";
+import { setFormData } from "../redux";
+import "./FormPage.css";
 
-const FormPage = ({userData, setUserData}) => {
-    const [submitted, setSubmitted] = useState(false);
+const validate = (values) => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  }
 
-    const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            phoneNumber: '',
-            email: '',
-        },
-        onSubmit: (values)=>{
-            setUserData(values)
-            console.log(values)
-            setSubmitted(true)
-        }
-    })
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  }
 
-    return (
-        <div className='marginTop'>
-            <h1 className='text-fill-form'>Please Fill the form</h1>
-            <div className='home-container'>
-            <form onSubmit={formik.handleSubmit}>
-                <input
-                    type="text"
-                    className='form-inp'
-                    placeholder='Please Enter First Name'
-                    {...formik.getFieldProps('firstName')}
-                /> <br/>
+  if (!values.phoneNumber) {
+    errors.phoneNumber = "Required";
+  }
 
-                <input
-                    type="text"
-                    className='form-inp'
-                    placeholder='Please Enter Last Name'
-                    {...formik.getFieldProps('lastName')}
-                /><br/>
+  if (!values.email) {
+    errors.email = "Required";
+  }
 
-                <input
-                    type="text"
-                    className='form-inp'
-                    placeholder='Please Enter Phone number'
-                    {...formik.getFieldProps('phoneNumber')}
-                /><br/>
+  return errors;
+};
 
-                <input
-                    type="text"
-                    className='form-inp'
-                    placeholder='Please Enter Email'
-                    {...formik.getFieldProps('email')}
-                /><br/>
-                <div 
-                style={{
-                    display: 'flex',
-                    justifyContent: 'space-around'
-                }}>
-                <Link to='/'><button className='submit-btn' >Back</button></Link>
-                <button className='submit-btn' type='submit'>Submit</button>
-                </div>
-                
-            </form>
+const FormPage = ({ setUserData }) => {
+  const [submitted, setSubmitted] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      email: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      setUserData(values);
+      console.log(values);
+      setSubmitted(true);
+    },
+  });
+
+  return (
+    <div className="marginTop">
+      <h1 className="text-fill-form">Please Fill the form</h1>
+      <div className="home-container">
+        <form onSubmit={formik.handleSubmit}>
+          <input
+            type="text"
+            className="form-inp"
+            id='firstName'
+            placeholder="Please Enter First Name"
+            {...formik.getFieldProps("firstName")}
+          />
+          <br />
+          <div className='error-msg'>
+            {formik.touched.firstName && formik.errors.firstName ? (
+              <div>{formik.errors.firstName}</div>
+            ) : null}
+          </div>
+          <input
+            type="text"
+            className="form-inp"
+            id='lastName'
+            placeholder="Please Enter Last Name"
+            {...formik.getFieldProps("lastName")}
+          />
+          <br />
+          <div className='error-msg'>
+          {formik.touched.lastName && formik.errors.lastName ? (
+              <div>{formik.errors.lastName}</div>
+            ) : null}
             </div>
-        {submitted && <Redirect to='/success' />}
-            
-        </div>
-    )
-}
 
-const mapStateToProps = (state) => {
-    return {
-      userData: state.users,
-    };
-  };
-  
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      setUserData: (values) => dispatch(setFormData(values)),
-    };
-  };
+          <input
+            type="text"
+            className="form-inp"
+            id='phoneNumber'
+            placeholder="Please Enter Phone number"
+            {...formik.getFieldProps("phoneNumber")}
+          />
+          <br />
+          <div className='error-msg'>
+          {formik.touched.phoneNumber && formik.errors.phoneNumber ? (
+            <div>{formik.errors.phoneNumber}</div>
+          ) : null}
+          </div>
 
-export default connect(mapStateToProps, mapDispatchToProps)(FormPage)
+          <input
+            type="text"
+            id="email"
+            className="form-inp"
+            placeholder="Please Enter Email"
+            {...formik.getFieldProps("email")}
+          />
+          <br />
+          <div className='error-msg'>
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+            }}
+          >
+            <Link to="/">
+              <button className="submit-btn">Back</button>
+            </Link>
+            <button className="submit-btn" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      {submitted && <Redirect to="/success" />}
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUserData: (values) => dispatch(setFormData(values)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(FormPage);
